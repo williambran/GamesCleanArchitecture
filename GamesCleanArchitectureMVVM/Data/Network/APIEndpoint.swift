640ab17e17e17030a3cc490be092b3322c837e2b
william <william.tepec@gmail.com>
2022-04-11 22:59:07 -0500

    se agrega el archivo faltante

---
+++ GamesCleanArchitectureMVVM/Data/Network/APIEndpoint.swift
@@ -0,0 +1,25 @@
+//
+//  APIEndpoint.swift
+//  GamesCleanArchitectureMVVM
+//
+//  Created by wito on 20/01/22.
+//
+
+import Foundation
+
+
+
+struct Endpoints {
+    
+    
+    static func login(loginRequestDTO: LoginRequestDTO ) -> EndPoint<LoginResponseDTO>{
+        
+        return EndPoint(path: "api/v1/auth", isFullPath: false, method: .POST, bodyParametersEncodable: loginRequestDTO)
+    }
+    
+    
+    static func getAllGames() ->EndPoint<GamesListDTO> {
+        
+        return EndPoint(path: "api/games", method: .GET)
+    }
+}
